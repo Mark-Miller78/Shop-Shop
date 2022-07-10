8627c1e78c24e38e69c80ff7a7f44b670c13dbcc
Mark Miller <markmiller7858@gmail.com>
2022-07-10 12:52:24 -0400

    starting switch to redux

--- client/src/index.js
+++ client/src/index.js
@@ -4,9 +4,18 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import {configureStore} from '@reduxjs/toolkit'
+import {Provider} from 'react-redux';
+
+const store = configureStore({
+  reducer:{}
+});
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
